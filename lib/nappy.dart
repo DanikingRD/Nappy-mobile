caf2d374d3a2d591d62437f5daf3fbd08ec771d2
DanikingRD <danikingrd@gmail.com>
2022-12-28 07:52:02 -0400

    redirect user after sign in

--- lib/nappy.dart
+++ lib/nappy.dart
@@ -1,37 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nappy_mobile/constants/assets.dart';
 import 'package:nappy_mobile/constants/colors.dart';
 import 'package:nappy_mobile/constants/styles.dart';
+import 'package:nappy_mobile/features/auth/controllers/auth_controller.dart';
 import 'package:nappy_mobile/router.dart';
 import 'package:routemaster/routemaster.dart';
 
-class Nappy extends StatelessWidget {
+class Nappy extends ConsumerWidget {
   const Nappy({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationParser: const RoutemasterParser(),
-      routerDelegate: RoutemasterDelegate(
-        routesBuilder: (context) => publicRoutes,
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authEvents = ref.watch(authStateChangesProvider);
+    return authEvents.when(
+      data: (optionalId) {
+        return MaterialApp.router(
+          routeInformationParser: const RoutemasterParser(),
+          routerDelegate: RoutemasterDelegate(
+            routesBuilder: (context) {
+              return optionalId.match(
+                () => publicRoutes,
+                (id) => appRoutes,
+              );
+            },
+          ),
+          theme: ThemeData(
+            colorScheme: const ColorScheme.light(
+              primary: NappyColors.primary,
+            ),
+            inputDecorationTheme: kPrimaryInputDecorationTheme,
+            scaffoldBackgroundColor: Colors.white,
+            primaryIconTheme: kPrimaryIconTheme,
+            iconTheme: kPrimaryIconTheme,
+            fontFamily: kPoppinsFont,
+            textTheme: TextTheme(
+              headline1: kHeadline1,
+              subtitle1: kSubtitle1,
+              bodyText1: kBodyText1,
+            ),
+            textSelectionTheme: kTextSelectionTheme,
+          ),
+          debugShowCheckedModeBanner: false,
+        );
+      },
+      error: (error, stack) => Scaffold(
+        body: Center(child: Text(error.toString())),
       ),
-      theme: ThemeData(
-        colorScheme: const ColorScheme.light(
-          primary: NappyColors.primary,
+      loading: () => const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
         ),
-        inputDecorationTheme: kPrimaryInputDecorationTheme,
-        scaffoldBackgroundColor: Colors.white,
-        primaryIconTheme: kPrimaryIconTheme,
-        iconTheme: kPrimaryIconTheme,
-        fontFamily: kPoppinsFont,
-        textTheme: TextTheme(
-          headline1: kHeadline1,
-          subtitle1: kSubtitle1,
-          bodyText1: kBodyText1,
-        ),
-        textSelectionTheme: kTextSelectionTheme,
       ),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/router.dart
+++ lib/router.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:nappy_mobile/features/auth/views/login_view.dart';
+import 'package:nappy_mobile/features/home/views/home_view.dart';
 import 'package:routemaster/routemaster.dart';
 
 final publicRoutes = RouteMap(
   routes: {
     '/': (_) => const MaterialPage(child: LoginView()),
+  },
+);
+
+final appRoutes = RouteMap(
+  routes: {
+    '/app/cards': (_) => const MaterialPage(child: HomeView()),
   },
 );
